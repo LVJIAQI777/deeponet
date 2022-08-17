5ab2a48853d4df9dcbe8830a9a0b818afa9125ba
LVJIAQI777 <15836117901@163.com>
2022-08-17 11:05:24 +0800

    update .gitignore

---
+++ Flow_field_DONet.py
@@ -0,0 +1,50 @@
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch"""
+import deepxde as dde
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load dataset
+data_path = "./data/"
+pic_path = "./pic/"
+model_path = "./model/"
+
+def generate_data(ratio):
+    x = np.loadtxt(data_path + 'U.txt')[:, 0]
+    U = np.loadtxt(data_path + 'U.txt')[:, 1]
+    T = np.loadtxt(data_path + 'T&Tv.txt')[:, 1]
+
+    N = x.shape[0]
+    N_train = int(ratio[0] * N)
+    N_valid = int(ratio[1] * N)
+    N_test = N - N_train - N_valid
+
+    data = np.stack((x, U, T), axis=-1)
+    # 需要打乱数据加上这一行
+    np.random.shuffle(data)
+
+    xx = data[:,0]
+    UU = data[:,1]
+    TT = data[:,2]
+    
+
+    # x:Branch and Trunk net input y:output
+    x_train = (data[0:N_train, 1], data[0:N_train, 0])
+    y_train = data[0:N_train, 2]
+
+    x_valid = (data[N_train:N_train+N_valid, 1],
+            data[N_train:N_train+N_valid, 0])
+    y_valid = data[N_train:N_train+N_valid, 2]
+
+    x_test = (data[-N_test:, 1], data[-N_test:, 0])
+    y_test = data[-N_test:, 2]
+    return x_train,y_train,x_valid,y_valid,x_test,y_test
+
+data_ratio = np.array([0.7,0.2,0.1])
+x_train,y_train,x_valid,y_valid,x_test,y_test = generate_data(data_ratio)
+
+
+
+data = dde.data.TripleCartesianProd(
+    X_train=x_train, y_train=y_train, X_test=x_valid, y_test=y_valid
+)
+
---
+++ U_x_DONet.py
@@ -0,0 +1,49 @@
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch"""
+import deepxde as dde
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load dataset
+data_path = "./data/"
+pic_path = "./pic/"
+model_path = "./model/"
+
+def generate_data(ratio):
+    x = np.loadtxt(data_path + 'U.txt')[:, 0]
+    U = np.loadtxt(data_path + 'U.txt')[:, 1]
+
+    N = x.shape[0]
+    N_train = int(ratio[0] * N)
+    N_valid = int(ratio[1] * N)
+    N_test = N - N_train - N_valid
+
+    data = np.stack((x, U), axis=-1)
+    # 需要打乱数据加上这一行
+    np.random.shuffle(data)
+
+    x_train = data[0:N_train, 0]
+    y_train = data[0:N_train, 1]
+
+    x_valid = data[N_train:N_train+N_valid, 0]
+    y_valid = data[N_train:N_train+N_valid, 1]
+
+    x_test = data[-N_test:, 0]
+    y_test = data[-N_test:, 1]
+
+    # 将1D数组变成2D
+    x_train = x_train.reshape(-1,1)
+    y_train = y_train.reshape(-1,1)
+    x_valid = x_valid.reshape(-1,1)
+    y_valid = y_valid.reshape(-1,1)
+    x_test = x_test.reshape(-1,1)
+    y_test = y_test.reshape(-1,1)
+    return x_train, y_train, x_valid, y_valid, x_test, y_test
+
+data_ratio = np.array([0.7,0.2,0.1])
+x_train,y_train,x_valid,y_valid,x_test,y_test = generate_data(data_ratio)
+
+
+
+data = dde.data.TripleCartesianProd(
+    X_train=x_train, y_train=y_train, X_test=x_valid, y_test=y_valid
+)
